366e5503845c7126f23a8e4cdf085e22a31d3bf0
unknown <ayman812001@gmail.com>
2021-11-18 15:14:01 +0200

    Tran

---
+++ resources/lang/en/general.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'Site_n' => 'Start Bootstrap',
+    'header_a' => 'About',
+    'header_s'=> 'Services',
+    'header_p'=>'Portfolio',
+    'header_c'=>'Contact',
+    'About_pa'=> 'Welcome To About Page',
+
+
+
+
+
+
+
+
+    // admin
+
+'Admin' => 'Dashboard'
+
+
+
+
+
+
+
+
+];
